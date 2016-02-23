344fa9098eeb1beed8509e570ca1eb7f5f18714f
mikeflowerdew <mikeflowerdew@gmail.com>
2016-02-23 17:11:04 +0100

    Loop over CLs values, to be continued

--- HistFitter/HistFitterLoop.py
+++ HistFitter/HistFitterLoop.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 
-import ROOT
+import ROOT, math
 
 class PaperResults:
 
@@ -9,6 +9,7 @@ class PaperResults:
         'Nbkg': None, # Could be a ValueWithError?
         'NbkgErr': None, # Absolute error
         'Ndata': None,
+        'Limit': None, # Model independent limit, to start the Nsig scan
         }
 
     def __init__(self):
@@ -131,7 +132,6 @@ def RunOneSearch(config, Nsig):
         # ########################
         # Step 3: Create a workspace
 
-        print '============= MJF RUNNING STEP 3'
         ROOT.gSystem.Exec('hist2workspace master.xml')
 
         # ########################
@@ -163,6 +163,67 @@ def RunOneSearch(config, Nsig):
     except:
         return None
 
+def NSigStrategy(existingResults, SRobj, granularity=0.02):
+    """Returns a list of signal yield values to try,
+    given a list of (Nsig,CLs) tuples.
+    SRobj is a PaperResults object.
+    The granularity argument is the desired CLs separation
+    - if larger gaps are found, yield values should be suggested to fill
+    in the gap.
+    May return an empty list in case of error or no more samples needed.
+    """
+
+    if not existingResults:
+        # Start with a simple list based on the model-independent limit
+        # Start with "high priority" runs close to exclusion.
+        return [scale*SRobj.Limit for scale in [1.0,0.5,2.0,0.25]]
+        
+    # Look through pairs of results for CLs values less than some threshold
+    # Neat trick from itertools!
+    from itertools import tee, izip
+    def pairwise(iterable):
+        "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+        a, b = tee(iterable)
+        next(b, None)
+        return izip(a, b)
+
+    existingResults.sort()
+    # Special case to make sure we get to high values
+    if existingResults[0][1] < 0.999:
+        existingResults.append( (0,1) )
+
+    for result0,result1 in pairwise(existingResults):
+
+        # How does the CLs difference compare to our desired tolerance?
+        CLsDiff = abs(result1[1] - result0[1])
+        if CLsDiff < granularity:
+            # OK, move on
+            continue
+
+        # Break up the truth yield difference into sections,
+        # by comparing the CLsDiff to the requested granularity.
+        # Pretty dumb approach, should be conservative?
+        NsigDiff = abs(result1[0] - result0[0])
+        # How many segments do we need?
+        Nsteps = math.ceil(CLsDiff/granularity)
+        StepSize = NsigDiff/Nsteps
+        
+        # result0 *should* have the smallest Nsig value, but just in case...
+        try: assert(result1[0] > result0[0])
+        except AssertionError:
+            from pprint import pprint
+            pprint(existingResults)
+            raise
+
+        MinNsig = result0[0]
+
+        # Now, return our list of suggestions
+        # Of course MinNsig+Nsteps*StepSize is intentially not included ;)
+        return [MinNsig+i*StepSize for i in range(1,int(Nsteps))]
+
+    # If we get here, I guess we're done!
+    return []
+
 if __name__=='__main__':
 
     ROOT.gROOT.SetBatch(True)
@@ -178,6 +239,38 @@ if __name__=='__main__':
     config.Ndata = 3
     config.Nbkg = 1.6
     config.NbkgErr = 0.5
+    config.Limit = 5.9
 
-    Nsig = 5.9
-    RunOneSearch(config, Nsig)
+    YieldValues = NSigStrategy([], config) # First list of variables
+    results = []
+
+    while YieldValues:
+
+        # Pick up the first yield value
+        Nsig = YieldValues.pop(0)
+
+        CLs = RunOneSearch(config, Nsig)
+        results.append( (Nsig,CLs) )
+
+        # If we're out of values, give a chance to replenish them
+        if not YieldValues:
+            YieldValues = NSigStrategy(results, config)
+
+    from pprint import pprint
+    pprint(results)
+
+    graph = ROOT.TGraph()
+    graph.SetName(config.SR+'_graph')
+    results.sort() # Just in case
+    for Nsig,CLs in results:
+        graph.SetPoint(graph.GetN(),CLs,Nsig)
+
+    # Amazingly this works!
+    function = ROOT.TF1(config.SR, lambda x,p: p[0]*graph.Eval(x[0]), 0, 1, 1)
+    function.SetParameter(0,1) # Default "normalisation"
+    
+    # Store TGraphs in an output file
+    outfile = ROOT.TFile.Open('CLsFunctions.root','RECREATE')
+    graph.Write()
+    function.Write()
+    outfile.Close()
