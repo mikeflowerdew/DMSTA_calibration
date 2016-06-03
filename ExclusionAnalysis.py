b86cccefe0fcb718d150cdb1bda4bb82d54bb7e9
Michael James Flowerdew <flowerde@mpp.mpg.de>
2016-06-03 13:15:18 +0200

    Add 2D plots and other tweaks

--- ExclusionAnalysis.py
+++ ExclusionAnalysis.py
@@ -3,6 +3,13 @@
 class SRresult:
 
     plotlist = ['mu','M_1','M_2','tanb'] \
+        + ['mu:M_1'] \
+        + ['M_2:M_1'] \
+        + ['mu:M_2'] \
+        + ['m_chi_10:m_chi_1p'] \
+        + ['m_chi_20:m_chi_1p'] \
+        + ['m_chi_30:m_chi_20'] \
+        + ['m_chi_30:m_chi_2p'] \
         + ['m_chi_%i0'%i for i in range(1,5)] \
         + ['m_chi_%ip'%i for i in range(1,3)] \
         + ['N_1%i'%i for i in range(1,5)] \
@@ -36,10 +43,30 @@ class SRresult:
         + ['BF_chi_2p_to_chi_1p%s'%s for s in ['','_Z','_h']] \
         + ['BF_chi_2p_other']
 
-    def __init__(self, eventlist=None, cutstrings=None):
+    cutranges = {
+        'All': {
+            'm_chi_20': 'abs(m_chi_20) < 1000',
+            'm_chi_30': 'abs(m_chi_30) < 1000',
+            },
+        'TwoLep': {
+            'mu': 'abs(mu) < 500',
+            'M_1': 'abs(M_1) < 200',
+            # 'mu:M_1': 'abs(mu) < 500 && abs(M_1) < 200',
+            'm_chi_30': 'abs(m_chi_30) < 500',
+            },
+        'ThreeLep': {
+            'mu': 'abs(mu) < 500',
+            'M_1': 'abs(M_1) < 200',
+            'M_2': 'abs(M_2) < 1000',
+            # 'mu:M_1': 'abs(mu) < 500 && abs(M_1) < 200',
+            'm_chi_30': 'abs(m_chi_30) < 500',
+            'm_chi_40': 'abs(m_chi_40) < 1000',
+            },
+        }
+
+    def __init__(self, eventlist=None):
         
         self.eventlist = eventlist
-        self.cutstrings = cutstrings if cutstrings else {}
         self.histograms = {}
 
     def getplots(self, tree, name='', templates=None):
@@ -48,7 +75,7 @@ class SRresult:
 
         for branchname in self.plotlist:
 
-            histname = '_'.join([branchname,name]) if name else branchname
+            histname = '_'.join([branchname.replace(':','_'),name]) if name else branchname
 
             joinstr = '>>'
             if templates and templates.has_key(branchname):
@@ -57,14 +84,22 @@ class SRresult:
                 self.histograms[branchname].Reset()
 
             try:
-                tree.Draw(branchname+joinstr+histname, self.cutstrings[branchname])
+                tree.Draw(branchname+joinstr+histname, self.cutranges[name][branchname])
             except KeyError:
                 tree.Draw(branchname+joinstr+histname)
             if not self.histograms.has_key(branchname):
                 self.histograms[branchname] = ROOT.gDirectory.Get(histname)
 
             self.histograms[branchname].SetDirectory(0)
-            self.histograms[branchname].GetXaxis().SetTitle(branchname)
+            if ':' in branchname:
+                # 2D plot
+                axisnames = branchname.split(':')
+                # Yes, these labels are the correct way around!
+                self.histograms[branchname].GetXaxis().SetTitle(axisnames[1])
+                self.histograms[branchname].GetYaxis().SetTitle(axisnames[0])
+            else:
+                # 1D plot
+                self.histograms[branchname].GetXaxis().SetTitle(branchname)
 
 if __name__=='__main__':
 
@@ -153,38 +188,47 @@ if __name__=='__main__':
 
     for plotname in SRresult.plotlist:
 
+        drawopt = 'BOX' if ':' in plotname else ''
+
+        if plotname.startswith('N_'):
+            for key in ['All','TwoLep','ThreeLep','FourLep']:
+                resultdict[key].histograms[plotname].GetXaxis().SetRangeUser(-1,1)
+
         canvas.cd(1)
-        resultdict['All'].histograms[plotname].Draw()
+        resultdict['All'].histograms[plotname].Draw(drawopt)
         ROOT.myText(0.2,0.9,ROOT.kBlack,'All evgen')
         resultdict['None'].histograms[plotname].SetLineWidth(0)
         resultdict['None'].histograms[plotname].SetLineColor(ROOT.kRed)
         resultdict['None'].histograms[plotname].SetFillColor(ROOT.kRed)
-        resultdict['None'].histograms[plotname].Draw('same')
+        resultdict['None'].histograms[plotname].Draw(drawopt+'same')
         ROOT.myText(0.2,0.85,ROOT.kRed,'Not excluded')
 
         canvas.cd(2)
-        resultdict['TwoLep'].histograms[plotname].Draw()
+        resultdict['TwoLep'].histograms[plotname].Draw(drawopt)
         resultdict['WWa'].histograms[plotname].SetLineWidth(0)
         resultdict['WWa'].histograms[plotname].SetLineColor(ROOT.kBlue)
         resultdict['WWa'].histograms[plotname].SetFillColor(ROOT.kBlue)
-        resultdict['WWa'].histograms[plotname].Draw('same')
+        resultdict['WWa'].histograms[plotname].Draw(drawopt+'same')
         ROOT.myText(0.2,0.9,ROOT.kBlack,'2L excluded')
+        ROOT.myText(0.2,0.85,ROOT.kBlue,'SR-WWa')
 
         canvas.cd(3)
-        resultdict['ThreeLep'].histograms[plotname].Draw()
+        resultdict['ThreeLep'].histograms[plotname].Draw(drawopt)
         resultdict['SR0a_16'].histograms[plotname].SetLineWidth(0)
         resultdict['SR0a_16'].histograms[plotname].SetLineColor(ROOT.kBlue)
         resultdict['SR0a_16'].histograms[plotname].SetFillColor(ROOT.kBlue)
-        resultdict['SR0a_16'].histograms[plotname].Draw('same')
+        resultdict['SR0a_16'].histograms[plotname].Draw(drawopt+'same')
         ROOT.myText(0.2,0.9,ROOT.kBlack,'3L excluded')
+        ROOT.myText(0.2,0.85,ROOT.kBlue,'SR0#taua bin 16')
 
         canvas.cd(4)
-        resultdict['FourLep'].histograms[plotname].Draw()
+        resultdict['FourLep'].histograms[plotname].Draw(drawopt)
         resultdict['SR0Z'].histograms[plotname].SetLineWidth(0)
         resultdict['SR0Z'].histograms[plotname].SetLineColor(ROOT.kBlue)
         resultdict['SR0Z'].histograms[plotname].SetFillColor(ROOT.kBlue)
-        resultdict['SR0Z'].histograms[plotname].Draw('same')
+        resultdict['SR0Z'].histograms[plotname].Draw(drawopt+'same')
         ROOT.myText(0.2,0.9,ROOT.kBlack,'4L excluded')
+        ROOT.myText(0.2,0.85,ROOT.kBlue,'SR0Z')
 
         canvas.Print('ExclusionAnalysis.pdf')
 
